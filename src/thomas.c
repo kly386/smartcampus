a010917b9494b1d75d87aa49271edf3b642d4f5c
俞行 施 <shiyuhang_job@163.com>
2025-06-18 16:15:50 +0800

    delete thomas.c, add thomas2.c

--- src/thomas.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int thomas_behavior() {
-	int i = 0;
-	printf("i=%d\n", i);
-
-	printf("Hello, world!\n");
-	return 0;
-}
---
+++ src/thomas2.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int test_func() {
+	printf("Test Function!\n");
+	return 0;
+}
